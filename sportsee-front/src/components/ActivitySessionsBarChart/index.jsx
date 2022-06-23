b49686472ab4fa2c1114c1a263fbbb68c5dd8d6d
Sylvie Melin <sy.melin@gmail.com>
2022-06-23 19:58:21 +0200

    work on ActivitySessionsBarChart

--- sportsee-front/src/components/ActivitySessionsBarChart/index.jsx
+++ sportsee-front/src/components/ActivitySessionsBarChart/index.jsx
@@ -1,18 +1,37 @@
-import { BarChart, Bar, Cell, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import PropTypes from 'prop-types'
+import '../../styles/ActivitySessionsBarChart.css'
+
+function CustomTooltipBarChart({ payload, label, active }) {
+    if (active) {
+        return (
+            <div className='customTooltipBarChart'>
+                <p className='cTBC__text'>{`${payload[0].value}kg`}</p>
+                <p className='cTBC__text'>{`${payload[1].value}Kcal`}</p>
+            </div>
+
+        )
+    }
+}
+
+
 
 function ActivitySessionsBarChart({activitySessions}) {
+    const numberOfDay = (day) => { 
+        return parseInt((day.split('-'))[2], 10)
+    }
+
     return (
         <ResponsiveContainer width="100%" height="100%">
-            <BarChart width={600} height={300} data={activitySessions}>
-                <CartesianGrid stroke="#DEDEDE" strokeDasharray="2" />
-                <XAxis dataKey="day" />
-                <YAxis yAxisId="right" orientation="left" />
-                <YAxis yAxisId="left" orientation="right" />
-                <Tooltip />
-                <Legend />
-                <Bar yAxisId="left" dataKey="kilogram" barSize={7} fill="#282D30" />
-                <Bar yAxisId="right" dataKey="calories" barSize={7} fill="#E60000" />
+            <BarChart data={activitySessions} width={835} height={320} margin={{top: 95, right: 29, left: 0, bottom: 23}} barGap="8" >
+                <CartesianGrid stroke="#DEDEDE" strokeDasharray="2 2 2" vertical={false} />
+                <XAxis dataKey='day' tickFormatter={numberOfDay} scale="point" padding={{ left: 10, right: 10 }} stroke="#DEDEDE" tick={{fill: '#9B9EAC', fontSize:'14', fontWeight:'500'}} dy={16} />
+                <YAxis yAxisId="left" orientation="right" axisLine={false} tickLine={false} tick={{fill: '#9B9EAC', fontSize:'14', fontWeight:'500'}} type="number" /*domain={['dataMin', 'dataMax']}*/ dx={45} />
+                <YAxis yAxisId="right" orientation="left" axisLine={false} tickLine={false} tick={false} />
+                <Tooltip content={<CustomTooltipBarChart />} /*cursor={true}*/ />
+                <Legend iconType='circley' iconSize='8' verticalAlign='middle' wrapperStyle={{ top: 24, left: 200, lineHeight: '40px', fontSize: '14', fontWeight: 500 }} />
+                <Bar yAxisId="left" dataKey="kilogram" name="Poids (kg)" barSize={7} radius={[3, 3, 0, 0]} fill="#282D30" />
+                <Bar yAxisId="right" dataKey="calories" name="Calories brulées (kCal)" barSize={7} radius={[3, 3, 0, 0]} fill="#E60000" />
             </BarChart>      
         </ResponsiveContainer>  
     )
