5e2ec2c3039ea64d6490e14058ca9fbfea0a43ae
Sylvie Melin <sy.melin@gmail.com>
2022-06-24 16:50:11 +0200

    PieChart added

---
+++ sportsee-front/src/components/ScorePieChart/index.jsx
@@ -0,0 +1,53 @@
+import { PieChart, Pie, Sector, Cell, Label, ResponsiveContainer } from 'recharts';
+import PropTypes from 'prop-types'
+import '../../styles/ScorePieChart.css'
+
+function SPCCustomLabel({todayScore}) {
+    return (
+        <g>
+            <foreignObject x={85} y={105} width={100} height={100}>
+            <div className='SPCCustomLabel'>
+               <p className='SPCscore'>{todayScore * 100}%</p>
+               <p className='SPCtext'>de votre objectif</p> 
+            </div>
+            </foreignObject>
+        </g>
+    )
+    
+}
+
+function ScorePieChart({todayScore}) {
+    const arrayScore = [{score: todayScore}, {score: 1 - todayScore}]
+    const colors = ["#FF0000", "#FBFBFB"];
+
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+                <Pie
+                    data={arrayScore}
+                    cx={125}
+                    cy={125}
+                    innerRadius={75}
+                    outerRadius={90}
+                    dataKey="score"
+                    startAngle={90}
+                    endAngle={450}
+                    stroke='#FBFBFB'
+                >
+                    {arrayScore.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={colors[index % colors.length]}  cornerRadius={50} />
+                    ))}
+                
+                    <Label content={<SPCCustomLabel todayScore={todayScore} />} position="center" />
+                </Pie>
+                <text x="30" y="24" dominantBaseline="hanging" fontSize="15" fontWeight="500">Score</text>
+            </PieChart>      
+       </ResponsiveContainer>
+    )
+}
+
+ScorePieChart.propTypes = {
+    todayScore: PropTypes.number
+}
+
+export default ScorePieChart
--- sportsee-front/src/pages/Profile/index.jsx
+++ sportsee-front/src/pages/Profile/index.jsx
@@ -3,10 +3,12 @@ import Welcome from "../../components/Welcome"
 import ActivitySessionsBarChart from "../../components/ActivitySessionsBarChart"
 import AverageSessionLineChart from "../../components/AverageSessionLineChart"
 import PerformanceRadarChart from "../../components/PerformanceRadarChart"
+import ScorePieChart from "../../components/ScorePieChart"
 import EnergySourcesCount from "../../components/EnergySourcesCount"
 import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS } from '../../utils/data/data'
 import '../../styles/Profile.css'
 
+const userMainData = USER_MAIN_DATA[0]
 const userName = USER_MAIN_DATA[0].userInfos.firstName
 const todayScore = USER_MAIN_DATA[0].todayScore
 const keyData = USER_MAIN_DATA[0].keyData
@@ -30,7 +32,9 @@ function Profile() {
                 </div>
                 <div className="graph3">
                 </div>
-                <div className="graph4">Score</div>  
+                <div className="graph4">
+                    <ScorePieChart todayScore={todayScore} />
+                </div>  
             </div>
             <EnergySourcesCount
                 keyData={keyData}
