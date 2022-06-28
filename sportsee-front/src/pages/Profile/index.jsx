5162a4fbacda361f908337adfcfbb458b24e8b23
Sylvie Melin <sy.melin@gmail.com>
2022-06-28 22:05:35 +0200

    work on get data from API (start)

--- sportsee-front/src/pages/Profile/index.jsx
+++ sportsee-front/src/pages/Profile/index.jsx
@@ -5,18 +5,30 @@ import AverageSessionLineChart from "../../components/AverageSessionLineChart"
 import PerformanceRadarChart from "../../components/PerformanceRadarChart"
 import ScorePieChart from "../../components/ScorePieChart"
 import EnergySourcesCount from "../../components/EnergySourcesCount"
-import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from '../../utils/data/data'
+//import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from '../../utils/data/data'
+import { useFetch } from '../../utils/hooks'
 import '../../styles/Profile.css'
-
+/*
 const userMainData = USER_MAIN_DATA[0]
 const userName = USER_MAIN_DATA[0].userInfos.firstName
 const todayScore = USER_MAIN_DATA[0].todayScore
 const keyData = USER_MAIN_DATA[0].keyData
 const activitySessions = USER_ACTIVITY[0].sessions
 const averageSessions = USER_AVERAGE_SESSIONS[0].sessions
-const performance = USER_PERFORMANCE[0]
+const performance = USER_PERFORMANCE[0]*/
 
 function Profile() {
+
+    const { data, isLoading, error } = useFetch('http://localhost:3000/user/12')
+    
+    if (isLoading === false) {
+        const userData = data.data
+        console.log("userData", userData)
+
+        return <div>{userData.todayScore}</div>
+    }
+
+/*
     return (
         <div className="dashboard">
             <Welcome
@@ -42,7 +54,7 @@ function Profile() {
                 keyData={keyData}
             />
         </div>
-    )
+    )*/
 }
 
 export default Profile
