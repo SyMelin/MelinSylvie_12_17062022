664653ba52e583603f181f545a112f81202174bc
Sylvie Melin <sy.melin@gmail.com>
2022-06-29 14:54:55 +0200

    work on API

--- sportsee-front/src/pages/Profile/index.jsx
+++ sportsee-front/src/pages/Profile/index.jsx
@@ -1,14 +1,14 @@
-import React from "react"
+
 import Welcome from "../../components/Welcome"
 import ActivitySessionsBarChart from "../../components/ActivitySessionsBarChart"
 import AverageSessionLineChart from "../../components/AverageSessionLineChart"
 import PerformanceRadarChart from "../../components/PerformanceRadarChart"
 import ScorePieChart from "../../components/ScorePieChart"
 import EnergySourcesCount from "../../components/EnergySourcesCount"
-import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from '../../utils/data/data'
-//import { useFetch } from '../../utils/hooks'
+//import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from '../../utils/data/data'
+import { useMainData, useActivity } from '../../api'
 import '../../styles/Profile.css'
-
+/*
 const userMainData = USER_MAIN_DATA[0]
 const userName = USER_MAIN_DATA[0].userInfos.firstName
 const todayScore = USER_MAIN_DATA[0].todayScore
@@ -16,20 +16,36 @@ const keyData = USER_MAIN_DATA[0].keyData
 const activitySessions = USER_ACTIVITY[0].sessions
 const averageSessions = USER_AVERAGE_SESSIONS[0].sessions
 const performance = USER_PERFORMANCE[0]
+*/
+
+
+
 
 function Profile() {
-/*
-    const { data, isLoading, error } = useFetch('http://localhost:3000/user/12')
+    const userMainData = useMainData()
+    console.log(userMainData)
+    const activity = useActivity()
     
-    if (isLoading === false) {
-        const userData = data.data
-        console.log("userData", userData)
-
-        return <div>{userData.todayScore}</div>
+    if (userMainData.userInfos && activity) {
+        return (
+            <div className="dashboard">
+                <Welcome
+                    userName={0.12}
+                    todayScore={userMainData.todayScore} //Route!!!
+                />
+                <div className="wrapper">
+                    <div className="graph1">
+                        <ActivitySessionsBarChart activitySessions={activity.sessions}/>
+                    </div>
+                </div>
+            </div>
+        )
     }
-    */
 
+    
+   
 
+/*
     return (
         <div className="dashboard">
             <Welcome
@@ -55,7 +71,7 @@ function Profile() {
                 keyData={keyData}
             />
         </div>
-    )
+    )*/
 }
 
 export default Profile
