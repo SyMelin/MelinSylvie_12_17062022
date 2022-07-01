1cf4a8359c0418ec73609f7e39bf79c47c797af7
Sylvie Melin <sy.melin@gmail.com>
2022-07-01 16:04:34 +0200

    RadarChart: position of ticks

--- sportsee-front/src/components/PerformanceRadarChart/index.jsx
+++ sportsee-front/src/components/PerformanceRadarChart/index.jsx
@@ -1,6 +1,7 @@
 
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer, PolarRadiusAxis } from 'recharts'
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts'
 import PropTypes from 'prop-types'
+import '../../styles/PerformanceRadarChart.css'
 
 function PerformanceRadarChart({performance}) {
     const kindNamesEN = performance.kind
@@ -18,11 +19,25 @@ function PerformanceRadarChart({performance}) {
         return kindNamesENtoFR[kindNamesEN[kind]]
     }
 
+    function customPolarAngleAxis({ payload, x, y, cx, cy, ...rest }) {
+        return (
+            <text
+                {...rest}
+                y={y + (y - 80) / 15}
+                x={x + (x - cx) / 100}
+                fontSize={12}
+                fontWeight={500}
+            >
+                {kindToName(payload.value)}    
+            </text> 
+        )
+    }
+
     return (
         <ResponsiveContainer width="100%" height="100%">
             <RadarChart cx="50%" cy="50%" outerRadius="70%" data={[...performance.data].reverse()} >
                 <PolarGrid stroke="#FFFFFF" radialLines={false} />
-                <PolarAngleAxis dataKey="kind" tickFormatter={kindToName} stroke="#FFFFFF" tickLine={false}  tick={{fill: '#FFFFFF', fontSize:'12', fontWeight:'500'}} />
+                <PolarAngleAxis dataKey="kind" tickFormatter={kindToName} stroke="#FFFFFF" tickLine={false} tick={props => customPolarAngleAxis(props)} />
                 <Radar name="userPerformance" dataKey="value" fill="#FF0101" fillOpacity={0.7}  />
             </RadarChart>
         </ResponsiveContainer>  
