8698d0e20659ced3edcf9100e6c2f830f7688f5e
Sylvie Melin <sy.melin@gmail.com>
2022-07-04 16:20:35 +0200

    work on tooltip's cursor

--- sportsee-frontend/src/components/ActivitySessionsBarChart/index.jsx
+++ sportsee-frontend/src/components/ActivitySessionsBarChart/index.jsx
@@ -1,4 +1,5 @@
 import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { useState } from 'react';
 import PropTypes from 'prop-types'
 import '../../styles/ActivitySessionsBarChart.css'
 
@@ -19,14 +20,35 @@ function ActivitySessionsBarChart({activitySessions}) {
         return parseInt((day.split('-'))[2], 10)
     }
 
+    const [cursorX, setCursorX] = useState(-1000)
+    const onMouseMove = hoveredData => {
+    // console.log(hoveredData);
+        if (hoveredData && hoveredData.activeCoordinate) {
+            const positionX =  hoveredData.activeCoordinate.x
+            setCursorX(positionX)
+            // console.log(positionX)
+        }
+    };
+
+    const onMouseOut = () => {
+        setCursorX(-1000)
+    };
+
     return (
         <ResponsiveContainer>
-            <BarChart data={activitySessions}/*width={835} height={320}*/ margin={{top: 95, right: 29, left: 0, bottom: 23}} barGap={8} >
+            <BarChart
+                data={activitySessions}/*width={835} height={320}*/
+                margin={{top: 95, right: 29, left: 0, bottom: 23}}
+                barGap={8}
+                onMouseMove={onMouseMove}
+                onMouseOut={onMouseOut}
+                >
                 <CartesianGrid stroke="#DEDEDE" strokeDasharray="2 2 2" vertical={false} />
                 <XAxis dataKey="day" tickFormatter={numberOfDay} padding={{ left: -45, right: -45 }} stroke="#DEDEDE" tick={{fill: '#9B9EAC', fontSize:'14', fontWeight:'500'}} dy={16} />
+                <rect width={56} height="100%" x={cursorX - 56/2} y={0} fill= "#C4C4C4" opacity={0.5} />
                 <YAxis yAxisId="left" orientation="right" axisLine={false} tickLine={false} tick={{fill: '#9B9EAC', fontSize:'14', fontWeight:'500'}} type="number" domain={['dataMin - 1', 'dataMax + 1']} dx={45} />
                 <YAxis yAxisId="right" orientation="left" axisLine={false} tickLine={false} tick={false} />
-                <Tooltip offset={20} cursor={{ fill: '#C4C4C4', opacity: 0.5, width: 56 }} content={<CustomTooltipBarChart />} />
+                <Tooltip offset={20} cursor={false} /*cursor={{ fill: '#C4C4C4', opacity: 0.5, width: 56 }}*/ content={<CustomTooltipBarChart />} />
                 <Legend
                     iconType='circle'
                     iconSize='8'
--- sportsee-frontend/src/components/AverageSessionLineChart/index.jsx
+++ sportsee-frontend/src/components/AverageSessionLineChart/index.jsx
@@ -1,9 +1,7 @@
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, Rectangle } from 'recharts';
+import { useState } from 'react';
 import PropTypes from 'prop-types'
 import '../../styles/CustomTooltip.css'
-
-const weekdays = ['L', 'M', 'M', 'J', 'V', 'S', 'D']
-
 
 function CustomTooltip({ payload, active }) {
     if (active) {
@@ -15,8 +13,17 @@ function CustomTooltip({ payload, active }) {
     }
   
     return null;
-  }
+}
 
+
+const CustomCursor = props => {
+  const { x, y, width, height, stroke } = props;
+  
+  return <rect fill="red" stroke="black" x={x} y={y} width={width} height={height}>
+    
+    
+  </rect>;
+}
 
 function AverageSessionLineChart({averageSessions}) {
   const dayShort = (day) => {
@@ -24,14 +31,51 @@ function AverageSessionLineChart({averageSessions}) {
     return weekdays[day - 1]
   }
 
+  const [perc, setPerc] = useState(0);
+  const [cursorX, setCursorX] = useState(-1000)
+  const onMouseMove = hoveredData => {
+   // console.log(hoveredData);
+    if (hoveredData && hoveredData.activePayload) {
+      const hoveredX = hoveredData.activePayload[0].payload.day;
+     // console.log('hoveredX', hoveredX)
+      const index = averageSessions.findIndex(d => d.day === hoveredX);
+      const positionX =  hoveredData.activeCoordinate.x
+      const percentage = ((averageSessions.length - index - 1) * 100) / (averageSessions.length - 1);
+
+      setCursorX(positionX)
+     // console.log(positionX)
+      setPerc(100 - percentage)
+     // console.log(perc)
+    }
+  };
+
+  const onMouseOut = () => {
+    setCursorX(-1000)
+    setPerc(0)
+  };
+
     return (
         <ResponsiveContainer>
-            <LineChart /*width={600} height={300}*/ data={averageSessions} margin={{ top: 80, right: 0, bottom: 50, left: -50 }}>
-                <Line type="natural" dataKey="sessionLength" stroke="#FFFFFF" strokeWidth={2} dot={false} activeDot={{ r: 4 }} />
+            <LineChart /*width={600} height={300}*/ 
+              data={averageSessions}
+              margin={{ top: 80, right: 0, bottom: 50, left: -50 }}
+              onMouseMove={onMouseMove}
+              onMouseOut={onMouseOut}
+              >
+                <defs>
+                  <linearGradient id="colorUv" x1="0%" y1="0" x2="100%" y2="0">
+                    <stop offset="0%" stopColor="rgb(255, 255, 255, 0.4032" />
+                    <stop offset={`${perc / 0.4032}%`} stopColor="#FFFFFF" />
+                    <stop offset={`${perc / 0.4032}%`} stopColor="#FFFFFF" />
+                    <stop offset={`${100 / 0.4032}%`} stopColor="#FFFFFF" />
+                  </linearGradient>
+                </defs>
+                <Line type="natural" dataKey="sessionLength" stroke="url(#colorUv)" strokeWidth={2} dot={false} activeDot={{ r: 4 }} />
                 <XAxis dataKey="day" tickFormatter={dayShort} axisLine={false} tickLine={false} tick={{fill: '#FFFFFF', fontSize:'12', fontWeight:'500', opacity:'0.5'}} dy={19.5} />
                 <YAxis axisLine={false} tickLine={false} tick={false} />
-                <Tooltip content={<CustomTooltip />} />
+                <Tooltip /*cursor={<CustomCursor />}*/ cursor={false} content={<CustomTooltip />} />
                 <text x="34" y="29" dominantBaseline="hanging" fontSize="15" fontWeight="500" fill="#FFFFFF" opacity={0.5}><tspan x="34" y="29">Durée moyenne des</tspan><tspan x="34" y="53">sessions</tspan></text>
+                <rect width="110%" height="100%" x={cursorX} y={0} fill= "#000000" opacity={0.1} />
             </LineChart>
         </ResponsiveContainer>
         
