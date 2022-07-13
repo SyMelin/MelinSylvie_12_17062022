8729721441936b3d222cc10e802de27682fc4063
Sylvie Melin <sy.melin@gmail.com>
2022-07-13 18:43:22 +0200

    BarChartTooltip height

--- sportsee-frontend/src/components/ActivitySessionsBarChart/index.jsx
+++ sportsee-frontend/src/components/ActivitySessionsBarChart/index.jsx
@@ -14,6 +14,13 @@ function CustomTooltipBarChart({ payload, active }) {
     }
 }
 
+function CustomTooltipCursorBarChart({ cursorX, chartWrapper, ratio }) {
+    console.log(ratio)
+    return (
+      <rect width={56} height={(chartWrapper * 0.383) - 170 + (170 * (1 - ratio) / 1.23)} x={cursorX - 56/2} y={100 * ratio} fill="#C4C4C4" opacity={0.5}/>
+    )
+  }
+
 function ActivitySessionsBarChart({activitySessions, chartWrapper, dimensions, ratio}) {
     const numberOfDay = (day) => { 
         return parseInt((day.split('-'))[2], 10)
@@ -48,17 +55,16 @@ function ActivitySessionsBarChart({activitySessions, chartWrapper, dimensions, r
                 >
                 <CartesianGrid stroke="#DEDEDE" strokeDasharray="2 2 2" vertical={false} />
                 <XAxis dataKey="day" tickFormatter={numberOfDay} padding={{ left: -45, right: -45 }} stroke="#DEDEDE" tick={{fill:'#9B9EAC', fontSize:'14', fontWeight:'500'}} dy={16 * ratio} />
-                <rect width={56} height={chartWrapper * 0.383 - 168} x={cursorX - 56/2} y={99} fill="#C4C4C4" opacity={0.5} />
                 <YAxis yAxisId="left" orientation="right" axisLine={false} tickLine={false} tick={{fill:'#9B9EAC', fontSize:'14', fontWeight:'500'}} type="number" domain={['dataMin - 1', 'dataMax + 1']} dx={40} />
                 <YAxis yAxisId="right" orientation="left" axisLine={false} tickLine={false} tick={false} />
-                <Tooltip /*offset={20}*/ position={{x: (cursorX < (chartWrapper - 200)) ? cursorX + 20 : cursorX - 50, y: cursorY - 80}} cursor={false} content={<CustomTooltipBarChart />} />
+                <Tooltip /*offset={20}*/ position={{x: (cursorX < (chartWrapper - 200)) ? cursorX + 20 : cursorX - 50, y: cursorY - 80}} cursor={<CustomTooltipCursorBarChart cursorX={cursorX} chartWrapper={chartWrapper} ratio={ratio} />} content={<CustomTooltipBarChart />} />
                 <Legend
                     layout='horizontal'
                     iconType='circle'
                     iconSize='8'
                     verticalAlign= 'middle'
                     align= 'right'
-                    wrapperStyle={{ top: 20, left: 44, lineHeight: '40px', fontSize: '14', fontWeight: 500 }}
+                    wrapperStyle={{ top: 20 * ratio, left: 44, lineHeight: '40px', fontSize: '14', fontWeight: 500 }}
                     formatter={(value) => <span style={{color: '#74798C', display: 'inline-block', margin: '0 32px 0 13px', padding: '2px 0 0 0'}}>{value}</span>}
                     />
                 <Bar yAxisId="left" dataKey="kilogram" name="Poids (kg)" barSize={7} radius={[3, 3, 0, 0]} fill="#282D30" />
