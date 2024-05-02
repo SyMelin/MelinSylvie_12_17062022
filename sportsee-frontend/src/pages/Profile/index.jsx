567c6b6153f325a7bc0e5126beb81ed7aec2cbf1
Sylvie Melin <sy.melin@gmail.com>
2024-05-02 21:02:53 +0200

    "Add GitHub Pages version with mocked data"

--- sportsee-frontend/src/pages/Profile/index.jsx
+++ sportsee-frontend/src/pages/Profile/index.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react"
 import Welcome from "../../components/Welcome"
 import NumbersWrapper from "../../components/NumbersWrapper"
-import urls from "../../utils/constantes/urls"
-import { useFetch, useHandleResize } from '../../utils/hooks'
+import urls from "../../utils/constantes/urls" // not used in the GitHub Pages version of this project
+import { useFetch, useHandleResize } from '../../utils/hooks' // useFetch is not used in the GitHub Pages version of this project
 import { User } from '../../models/user'
+import mockedData from "../../utils/constantes/mockedData" // NOTE: This data import is for showcase purposes only and replaces API calls in the GitHub Pages version of this project.
 import '../../styles/Profile.css'
 
 
@@ -15,13 +15,34 @@ import '../../styles/Profile.css'
  */
 function Profile() {
     const { userId } = {userId: 12} //to be substituted by " const {userId}  = useParams() " when the router is set
-    const { data, isLoading, error } = useFetch(Object.values(urls(userId)))
+    // const { data, isLoading, error } = useFetchData(urls(userId)) // when the server is set. Not used in the GitHub Pages version of this project
+    const isLoading = false // workaround specifically for the GitHub Pages version of this project
+    const error = false // workaround specifically for the GitHub Pages version of this project
+    /**
+     * Note: As GitHub Pages doesn't support server-side operations, the data that would normally be fetched 
+     * from the server using axios is instead imported directly from a local mock data file. 
+     * This is a workaround specifically for the GitHub Pages version of this project and is not representative 
+     * of how the application functions in a production environment.
+     */
+    const { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } = mockedData;  // only for showcase purposes on GitHub Pages
+    // getUserData mocks the data received from the server for the user with the id 'userId'
+    const getUserData = (id) => {
+        const mainData = USER_MAIN_DATA.find(user => user.id === id);
+        return [
+            mainData,
+            USER_ACTIVITY.find(user => user.userId === id),
+            USER_AVERAGE_SESSIONS.find(user => user.userId === id),
+            USER_PERFORMANCE.find(user => user.userId === id),
+            mainData.todayScore,
+            mainData.keyData
+        ]
+    }
+    const data = getUserData(userId) // only for showcase purposes on GitHub Pages
     
     // EVENT ON RESIZE :
     // Recharts' ResponsiveContainer not working
-    // so use the state variable 'windowDimensions' and th e hook useHandleResize()
+    // so use the state variable 'windowDimensions' and the hook useHandleResize()
     const { windowDimensions } = useHandleResize()
-
 
     if (error) {
         return <div>Error: Something went wrong</div>
@@ -32,7 +53,10 @@ function Profile() {
             //const [ mainData, activity, averageSessions, activities, todayScore, keyData ] = data
             //const user = { mainData, activity, averageSessions, activities, todayScore, keyData }
         */
+
+        console.log("data", data)
         const user = new User(data) // modification following the project's presentation
+        console.log("user", user)
 
         return (
             <div className="dashboard">
